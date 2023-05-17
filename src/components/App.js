606a2135ee9196c4fe7c424df0efdeeae6f6a75b
Konstantin Bartosh <rustigillo@gmail.com>
2023-05-17 17:08:24 +0600

    Feat: added handleCardLike function to App

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ function App() {
 		// Отправляем запрос в API и получаем обновлённые данные карточки
 		api.changeLikeCardStatus(card._id, !isLiked)
 			.then((newCard) => {
-				setCards((cards) => cards.map((c) => c._id === card._id ? newCard : c));
+				setCards((cards) => cards.map((item) => item._id === card._id ? newCard : item));
 			});
 	}
 
